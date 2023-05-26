6adff72aa92bbe27232db73c57763461cc9690db
Roy C <61641010+canacosmic@users.noreply.github.com>
2023-05-25 23:03:59 -0600

    creacion del proyecto
    se ha creado  el inico y la bienvenida. galeria por trabajar

---
+++ Bienvenida/App.js
@@ -0,0 +1,5 @@
+/* esto hace que al hacer click en cualquier parte de la pantalla se 
+rediriga al usuario hacia la pagina de inico */
+document.addEventListener('click', function () {
+    window.location.href = '../Inicio/inicio.html'; // Redirecciona al archivo index.html
+});
